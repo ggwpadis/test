06b95ee67842f52e28604d21d3c2ca85abce90f3
Zarylbekov Adis <zarylbekov_a@IUCA.LOC>
2025-01-28 12:36:33 +0600

    db

--- web/Web/Program.cs
+++ web/Web/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add services for database operations and authentication
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
